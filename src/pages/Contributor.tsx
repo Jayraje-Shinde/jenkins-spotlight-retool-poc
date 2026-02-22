5b78a61f962a97909b7f8271425175b462725245
Jayraje Shinde <zeyraje@gmail.com>
2026-02-22 17:25:50 +0530

    Done with adding functionality to fuzzy find contributors and also made a page that displays pure html of single contributors

--- src/pages/Contributor.tsx
+++ src/pages/Contributor.tsx
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom"
 import { contributorsArray } from "../data/contributors";
+import DOMPurify from "dompurify";
 export default function Contributor(){
-	const {slug } = useParams();
-		console.log(contributorsArray);
+	const { slug } = useParams();
+	const john = {
+		name : "john doe",
+		location : "Berlin",
+		intro : "Iam Fake person",
+		html : "<div>Random HTML</div>"
+	}
+	const contributor = contributorsArray.find(item => item.slug === slug) || john ;
 	return <>
-{slug} <br />
-this is contributors pages
+	<h1>{contributor.name}</h1>
+<span>{contributor.location}</span>
+<p>Intro : {contributor.intro}</p>
+<div dangerouslySetInnerHTML={{
+	__html : DOMPurify.sanitize(contributor?.html ?? "")
+	}} />
+
 	</>
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,16 +1,37 @@
+import {useMemo,useState} from 'react'
 import {contributorsArray} from "../data/contributors";
 import {useNavigate} from 'react-router-dom'
+import Fuse from 'fuse.js';
+
 export default function LandingPage() {
-	
+	 const [query, setQuery] = useState("");
 	const navigate = useNavigate();
+	const fuse = useMemo(() => {
+    return new Fuse(contributorsArray, {
+      keys: ["slug", "name", "location"], // keys to search in
+      threshold: 0.5 // adjust to control fuzziness
+    });
+  }, [contributorsArray]);
+
+
+const results = useMemo(() => {
+    if (!query) return contributorsArray; // Return full list if no query
+
+    return fuse.search(query).map(result => result.item);
+  }, [fuse, query, contributorsArray]);
 
 	return <>
-		{
-			 contributorsArray.map((contributor) => (
-        <button onClick={() => navigate(`/contributors/${contributor.slug}`)} key={contributor.slug}>
-          {contributor.name}
+		<input value={query} type="text" onChange={(e) => setQuery(e.target.value)} />
+		<ul>
+        {results.map(item => (
+			
+          <li key={item.slug}>
+				<button onClick={() => navigate(`/contributors/${item.slug}`)} key={item.slug}>
+          {item.name}
         </button>
-      ))
-		}
+          </li>
+        ))}
+      </ul>
+
 	</>
 }
