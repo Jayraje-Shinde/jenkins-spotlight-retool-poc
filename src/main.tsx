cd2cfccfb97d89cce61606472e5957f151b1d34e
Jayraje Shinde <zeyraje@gmail.com>
2026-02-24 11:22:04 +0530

    Cleaning Repo

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { Analytics } from "@vercel/analytics/next"
 import {theme } from './theme/theme.ts'
 import '@jenkinsci/jenkins-io-components';
 import { CssBaseline, ThemeProvider } from '@mui/material'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={theme} >
 	<CssBaseline/>
-	<Analytics />
     <App />
   </ThemeProvider>
   </BrowserRouter>,
